efe8ea6e43c50d4c8b6e98f84ae33e7cba5e6b41
Aleksander <aleksanderkotowicz@gmail.com>
2018-07-17 13:25:48 +0200

    initial project

---
+++ src/main/java/com/example/codeball/CodeballApplication.java
@@ -0,0 +1,12 @@
+package com.example.codeball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeballApplication.class, args);
+	}
+}
